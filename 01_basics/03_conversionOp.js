61938278bba808df89c7806b44161ad04f80d3d5
Aryan <aryanjha813@gmail.com>
2024-06-11 12:29:44 +0000

    add third file

---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,11 @@
+let score = "33a"
+
+console.log(typeof score);
+console.log((typeof(score)));
+
+let valueInNo = Number(score);
+
+console.log(typeof valueInNo);
+console.log(valueInNo);
+
+// "33" can be converted to a no but "33a" cannot and will fetch NaN
