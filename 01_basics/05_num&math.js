da81ef556cd52a3565404cde204f4fa03ae6ce59
Aryan <aryanjha813@gmail.com>
2024-06-11 18:55:51 +0000

    num and math file added

---
+++ 01_basics/05_num&math.js
@@ -0,0 +1,20 @@
+let score = 400;
+console.log(score);
+
+const balance = new Number(100); //same as strings, we get methods in this
+console.log(balance);
+
+console.log(balance.toString()); //converted to string so string methods available now
+console.log(balance.toFixed(2)); //upto 2 decimal points
+
+//++++++++++++++++++++++++++++++ Math +++++++++++++++++
+
+console.log(Math);
+console.log(Math.sqrt(25));
+console.log((Math.abs(-25)));
+console.log((Math.ceil(4.2)));
+console.log((Math.floor(4.2)));
+console.log((Math.min(4,2,1,3,9)));
+console.log((Math.max(4,2,1,3,9)));
+
+
