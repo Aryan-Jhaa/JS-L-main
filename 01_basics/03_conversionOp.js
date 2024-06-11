43c2ed55d0bad8a226682d172639342036d48d1a
Aryan <aryanjha813@gmail.com>
2024-06-11 13:03:00 +0000

    post and pre inc added

--- 01_basics/03_conversionOp.js
+++ 01_basics/03_conversionOp.js
@@ -9,3 +9,10 @@ console.log(typeof valueInNo);
 console.log(valueInNo);
 
 // "33" can be converted to a no but "33a" cannot and will fetch NaN
+
+let gameCounter = 100;
+
+console.log("gameCounter " + gameCounter);
+console.log("After post inc " + gameCounter++);  
+console.log("current value: " + gameCounter);
+console.log("After pre inc " + ++gameCounter);  
