14acd6b5a1675f0ea2de544679e1d7136b5f561c
Aryan <aryanjha813@gmail.com>
2024-06-14 13:23:20 +0000

    truthy file updated

--- 04_control_flow/03_truthy.js
+++ 04_control_flow/03_truthy.js
@@ -30,3 +30,23 @@ const emptyObj = {};
 if (Object.keys(emptyObj).length === 0) {
     console.log("Object is empty");
 }
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+val1 = 5 ?? 10;
+console.log(val1);
+
+val1 = null ?? 15;
+console.log(val1);
+
+val1 = null ?? 5 ?? 15; //the first acceptable value will be taken 
+console.log(val1);
+
+// Termiary Operator
+
+// condition ? true : false
+
+const iceTea = 100;
+
+iceTea <=80 ? console.log("less than 80") : console.log("more than 80");
