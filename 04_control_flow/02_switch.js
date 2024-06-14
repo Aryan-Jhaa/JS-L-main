676f742af26d2214ec26af0c53bc99fb5ffb2d36
Aryan <aryanjha813@gmail.com>
2024-06-14 13:06:00 +0000

    switch file added

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,19 @@
+const month = 2;
+let monthName = undefined;
+switch (month) {
+    case 1:
+        monthName = "January";
+        console.log(monthName);
+        break;
+    case 2:
+        monthName = "February";
+        console.log(monthName);
+        break;
+    case 3:
+        monthName = "March";
+        console.log(monthName);
+        break;
+    default:
+        console.log("Default case for month value above 3");
+        break;
+}
